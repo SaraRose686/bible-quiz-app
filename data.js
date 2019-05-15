ff2212f812db778a4600d343a9e92af6e130a42d
Sara Eide-Altman <SaraRose686@gmail.com>
2019-05-15 00:21:52 -0700

    Updates to CSS and started JS files

---
+++ data.js
@@ -0,0 +1,62 @@
+export const QUESTIONS = [
+    {   // Question 1
+        question: `What was Sarah's reaction to the news that she would bear a son?`,
+        answers: [ `She laughed`, `She cried`, `She rejoiced`, `She fainted`],
+        correctAnswer: `She laughed`,
+        bibleText: `Genesis 18:12-13`
+    },
+    {   // Question 2
+        question: `Who was NOT one of the three Hebrew boys cast into the fiery furnace?`,
+        answers: [ `Shadrach`, `Meshach`, `Abed-nego`, `Daniel`],
+        correctAnswer: `Daniel`,
+        bibleText: `Daniel 3:20`
+    },
+    {   // Question 3
+        question: `Who was David's best friend?`,
+        answers: [ `Jonathan`, `Saul`, `Samuel`, `Goliath`],
+        correctAnswer: `Jonathan`,
+        bibleText: `1 Samuel 20:42`
+    },
+    {   // Question 4
+        question: `Who is NOT currently to heaven with God?`,
+        answers: [ `Enoch`, `Elijah`, `Moses`, `Solomon`],
+        correctAnswer: `Solomon`,
+        bibleText: `Genesis 5:24; 2 Kings 2:11; Matthew 17:3`
+    },
+    {   // Question 5
+        question: `Who was cast into a den of lions?`,
+        answers: [ `Daniel`, `Joseph`, `Elisha`, `Job`],
+        correctAnswer: `Daniel`,
+        bibleText: `Daniel 6:16`
+    },
+    {   // Question 6
+        question: `Where was Jesus mostly raised as a child?`,
+        answers: [ `Jerusalem`, `Bethlehem`, `Nazareth`, `Egypt`],
+        correctAnswer: `Nazareth`,
+        bibleText: `Matthew 2:23`
+    },
+    {   // Question 7
+        question: `What was the 12-year-old Jesus doing when His parents found Him?`,
+        answers: [ `Praying to His Father`, `Fishing with nets`, `Teaching in the temple`, `Playing with other children`],
+        correctAnswer: `Teaching in the temple`,
+        bibleText: `Luke 2:46-47`
+    },
+    {   // Question 8
+        question: `What happened immediately after Peter denied Christ three times?`,
+        answers: [ `Peter fell dead`, `The ground shook`, `Peter went blind`, `A rooster crowed`],
+        correctAnswer: `A rooster crowed`,
+        bibleText: `Matthew 26:74`
+    },
+    {   // Question 9
+        question: `When Jesus died on the cross, which of the following things did NOT happen?`,
+        answers: [ `There was an earthquake`, `Jesus came back to life`, `The dead came to life`, `The veil of the temple was torn`],
+        correctAnswer: `Jesus came back to life`,
+        bibleText: `Matthew 27:51-53`
+    },
+    {   // Question 10
+        question: `What animal attacked Paul after being shipwrecked on the island of Malta?`,
+        answers: [ `Viper`, `Bear`, `Mule`, `Scorpion`],
+        correctAnswer: `Viper`,
+        bibleText: `Acts 28:3`
+    }
+]
---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import { QUESTIONS } from "./data.js";
+
+
+
+
+
+const handleQuiz = () => {
+    console.log("handleQuiz() ran");
+    
+};
+
+$(handleQuiz);
